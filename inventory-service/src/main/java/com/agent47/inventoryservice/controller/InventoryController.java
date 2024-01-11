3cbcf24cc954243d92569738064d9ad4589db7ca
Uchhas <uchhasmacap@gmail.com>
2024-01-11 17:26:52 +0600

    I just added spring.cloud.compatibility-verifier.enable = false per to the suggestion i got from terminal on all the services i think it is working

--- inventory-service/src/main/java/com/agent47/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/agent47/inventoryservice/controller/InventoryController.java
@@ -17,7 +17,7 @@ public class InventoryController {
     //http://localhost:8090/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam("skuCode") List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
